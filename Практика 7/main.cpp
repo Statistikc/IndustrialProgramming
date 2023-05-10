52ad8ff83ca15e091196d9edd9473b2db0f128a1
Statistikc <94199217+Statistikc@users.noreply.github.com>
2023-05-10 21:17:27 +0300

    Add files via upload

---
+++ Практика 7/main.cpp
@@ -0,0 +1,40 @@
+﻿#include <iostream>
+#include <cmath>
+#include "type1.cpp"
+#include "type2.cpp"
+
+int main()
+{
+    int equationType;
+    std::cout << "Выберите тип уравнения:" << std::endl;
+    std::cout << "1. 0 = 0" << std::endl;
+    std::cout << "2. ax^2 = 0" << std::endl;
+
+    switch (equationType)
+    {
+    case 1:
+    {
+        type1 Solver;
+        Solver.Get_answer();
+        Solver.show();
+        break;
+    }
+    case 2:
+        {
+        double a;
+        std::cout << "Введите значение a: ";
+        std::cin >> a;
+
+        type2 Solver(a);
+        Solver.Get_answer();
+        Solver.show();
+        break;
+        }
+
+    default:
+        std::cout << "Неверный номер типа уравнения." << std::endl;
+        break;
+    }
+
+    return 0;
+}
